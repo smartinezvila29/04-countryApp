6e3a74cfedff6d9986de43a3713389c1ab8d1e59
Santiago Joel Martinez Vila <smartinezvila29@gmail.com>
2023-08-22 20:25:27 +0200

    routing childs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path:'contact',
     component: ContactPageComponent
     },
+    {
+      path:'countries',
+      loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule)
+      },
   {
     path:'**',
     redirectTo: 'home'
